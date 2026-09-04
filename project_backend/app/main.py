d1434a01f879defaf515654c981740cf2ee1e8b3
Impano Cyusa Chrispin <cyusaimpanochrispin@gmail.com>
2026-09-04 23:43:03 +0200

    This is the third commit

--- project_backend/app/main.py
+++ project_backend/app/main.py
@@ -27,7 +27,7 @@ def get_application() -> FastAPI:
         CORSMiddleware,
 
         allow_origins=[
-            "http://localhost:5173"
+            "http://localhost:3000"
         ],
 
         allow_credentials=True,
